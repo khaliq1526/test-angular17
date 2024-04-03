1e64a4047df960fea52099eed9ef1f51597bdaa0
khaliq1526 <khaliq1526@gmail.com>
2024-04-03 17:36:29 -0500

    Routes Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'users',
+        component: UsersComponent
+    },
+    {
+        path: 'orders',
+        component: OrdersComponent
+    },
+    {
+        path: 'products',
+        component: ProductsComponent
+    },
+
+];
--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { LeftMenuComponent } from '../components/left-menu/left-menu.component';
 
 @NgModule({
   declarations: [HeaderComponent, LeftMenuComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterLink
   ],
   exports:[HeaderComponent, LeftMenuComponent]
 })
