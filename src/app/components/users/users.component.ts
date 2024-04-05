2cb547d09dee1968538fc704b586fd47ce0cf8c4
khaliq1526 <khaliq1526@gmail.com>
2024-04-05 17:41:22 -0500

    4/5/2024

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,207 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../modules/shared.module';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+  users = [
+    {
+      id:1,
+      name: 'Tom',
+      mobile: '1234',
+      city:'city 1',
+      email:'ab@cd.com',
+      status: 'Active'
+    },
+    {
+      id:2,
+      name: 'Harry',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:3,
+      name: 'Majnu',
+      mobile: '73838',
+      city:'city 3',
+      email:'abc@gmail.com',
+      status: 'Active'
+    },
+    {
+      id:4,
+      name: 'Mahesh',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:1,
+      name: 'Tom',
+      mobile: '1234',
+      city:'city 1',
+      email:'ab@cd.com',
+      status: 'Active'
+    },
+    {
+      id:2,
+      name: 'Harry',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:3,
+      name: 'Majnu',
+      mobile: '73838',
+      city:'city 3',
+      email:'abc@gmail.com',
+      status: 'Active'
+    },
+    {
+      id:4,
+      name: 'Mahesh',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:1,
+      name: 'Tom',
+      mobile: '1234',
+      city:'city 1',
+      email:'ab@cd.com',
+      status: 'Active'
+    },
+    {
+      id:2,
+      name: 'Harry',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:3,
+      name: 'Majnu',
+      mobile: '73838',
+      city:'city 3',
+      email:'abc@gmail.com',
+      status: 'Active'
+    },
+    {
+      id:4,
+      name: 'Mahesh',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:1,
+      name: 'Tom',
+      mobile: '1234',
+      city:'city 1',
+      email:'ab@cd.com',
+      status: 'Active'
+    },
+    {
+      id:2,
+      name: 'Harry',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:3,
+      name: 'Majnu',
+      mobile: '73838',
+      city:'city 3',
+      email:'abc@gmail.com',
+      status: 'Active'
+    },
+    {
+      id:4,
+      name: 'Mahesh',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },{
+      id:1,
+      name: 'Tom',
+      mobile: '1234',
+      city:'city 1',
+      email:'ab@cd.com',
+      status: 'Active'
+    },
+    {
+      id:2,
+      name: 'Harry',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:3,
+      name: 'Majnu',
+      mobile: '73838',
+      city:'city 3',
+      email:'abc@gmail.com',
+      status: 'Active'
+    },
+    {
+      id:4,
+      name: 'Mahesh',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:1,
+      name: 'Tom',
+      mobile: '1234',
+      city:'city 1',
+      email:'ab@cd.com',
+      status: 'Active'
+    },
+    {
+      id:2,
+      name: 'Harry',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    },
+    {
+      id:3,
+      name: 'Majnu',
+      mobile: '73838',
+      city:'city 3',
+      email:'abc@gmail.com',
+      status: 'Active'
+    },
+    {
+      id:4,
+      name: 'Mahesh',
+      mobile: '7860',
+      city:'city 2',
+      email:'xy@cd.com',
+      status: 'Pending'
+    }
+  ];
 
 }
--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+//Ant Zoro 
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { AntZorroModule } from './ant-zorro.module';
+
+
 import { HeaderComponent } from '../components/header/header.component';
 import { LeftMenuComponent } from '../components/left-menu/left-menu.component';
 
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 @NgModule({
   declarations: [HeaderComponent, LeftMenuComponent],
   imports: [
-
-    CommonModule, RouterLink
-
+    CommonModule, RouterLink, AntZorroModule
   ],
-  exports:[HeaderComponent, LeftMenuComponent]
+  exports:[HeaderComponent, LeftMenuComponent, AntZorroModule],
+  providers: [ { provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons } ]
 })
 export class SharedModule { }
