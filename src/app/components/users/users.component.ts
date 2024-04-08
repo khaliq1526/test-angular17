c33df2e3a902712f60e6a0b84904fdf2406d5910
khaliq1526 <khaliq1526@gmail.com>
2024-04-08 17:28:42 -0500

    4/8/2024

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../modules/shared.module';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-users',
@@ -9,199 +10,18 @@ import { SharedModule } from '../../modules/shared.module';
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
-  users = [
-    {
-      id:1,
-      name: 'Tom',
-      mobile: '1234',
-      city:'city 1',
-      email:'ab@cd.com',
-      status: 'Active'
-    },
-    {
-      id:2,
-      name: 'Harry',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:3,
-      name: 'Majnu',
-      mobile: '73838',
-      city:'city 3',
-      email:'abc@gmail.com',
-      status: 'Active'
-    },
-    {
-      id:4,
-      name: 'Mahesh',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:1,
-      name: 'Tom',
-      mobile: '1234',
-      city:'city 1',
-      email:'ab@cd.com',
-      status: 'Active'
-    },
-    {
-      id:2,
-      name: 'Harry',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:3,
-      name: 'Majnu',
-      mobile: '73838',
-      city:'city 3',
-      email:'abc@gmail.com',
-      status: 'Active'
-    },
-    {
-      id:4,
-      name: 'Mahesh',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:1,
-      name: 'Tom',
-      mobile: '1234',
-      city:'city 1',
-      email:'ab@cd.com',
-      status: 'Active'
-    },
-    {
-      id:2,
-      name: 'Harry',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:3,
-      name: 'Majnu',
-      mobile: '73838',
-      city:'city 3',
-      email:'abc@gmail.com',
-      status: 'Active'
-    },
-    {
-      id:4,
-      name: 'Mahesh',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:1,
-      name: 'Tom',
-      mobile: '1234',
-      city:'city 1',
-      email:'ab@cd.com',
-      status: 'Active'
-    },
-    {
-      id:2,
-      name: 'Harry',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:3,
-      name: 'Majnu',
-      mobile: '73838',
-      city:'city 3',
-      email:'abc@gmail.com',
-      status: 'Active'
-    },
-    {
-      id:4,
-      name: 'Mahesh',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },{
-      id:1,
-      name: 'Tom',
-      mobile: '1234',
-      city:'city 1',
-      email:'ab@cd.com',
-      status: 'Active'
-    },
-    {
-      id:2,
-      name: 'Harry',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:3,
-      name: 'Majnu',
-      mobile: '73838',
-      city:'city 3',
-      email:'abc@gmail.com',
-      status: 'Active'
-    },
-    {
-      id:4,
-      name: 'Mahesh',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:1,
-      name: 'Tom',
-      mobile: '1234',
-      city:'city 1',
-      email:'ab@cd.com',
-      status: 'Active'
-    },
-    {
-      id:2,
-      name: 'Harry',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    },
-    {
-      id:3,
-      name: 'Majnu',
-      mobile: '73838',
-      city:'city 3',
-      email:'abc@gmail.com',
-      status: 'Active'
-    },
-    {
-      id:4,
-      name: 'Mahesh',
-      mobile: '7860',
-      city:'city 2',
-      email:'xy@cd.com',
-      status: 'Pending'
-    }
-  ];
+export class UsersComponent implements OnInit { 
+
+  users:any = [];
+  constructor(private userService: UsersService) {
+  }
+
+  ngOnInit(): void {   
+    this.userService.getUsers().subscribe(response => {
+      this.users = response;
+    }); 
+  }
+
+
 
 }
