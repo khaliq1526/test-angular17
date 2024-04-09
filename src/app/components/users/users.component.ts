19b0960721d15c17271ec0ebaabf0f0114705323
Ahteshamnayyar <ahteshamnayyar21@gmail.com>
2024-04-09 15:55:12 -0500

    Practice

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import { RouterLink } from '@angular/router';
+import { SharedModule } from '../../modules/shared.module';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,SharedModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+
+ users= [
+    { 
+      ID:1,
+      NAME:'Tom',
+      MOBILE:123,
+      CITY:'Houston',
+      EMAIL: '123@gmail.com',
+      STATUS: 'Active'
+  },
+  { 
+    ID:2,
+    NAME:'Harry',
+    MOBILE:456,
+    CITY:'Hamshpire',
+    EMAIL: '456@gmail.com',
+    STATUS: 'Pending'
+},
+{ 
+  ID:3,
+  NAME:'Monty',
+  MOBILE:789,
+  CITY:'Holland',
+  EMAIL: '789@gmail.com',
+  STATUS: 'Active'
+},
+{ 
+  ID:4,
+  NAME:'Johnny',
+  MOBILE:1011,
+  CITY:'Hungary',
+  EMAIL: '1011@gmail.com',
+  STATUS: 'Active'
+}
+]
 
 }
--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { LeftMenuComponent } from '../components/left-menu/left-menu.component';
+//Ant zorro
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { AntZorroModule } from './ant-zorro.module';
 
 
 @NgModule({
   declarations: [HeaderComponent, LeftMenuComponent],
   imports: [
 
-    CommonModule, RouterLink
+    CommonModule, RouterLink,AntZorroModule
 
   ],
-  exports:[HeaderComponent, LeftMenuComponent]
+  exports:[HeaderComponent, LeftMenuComponent,AntZorroModule]
 })
 export class SharedModule { }
